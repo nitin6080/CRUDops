7d682a258f29a0e576e1c7e2e42293c04c7c290c
NitinKumarShukla <nitin.kumar@vmatechlabs.com>
2024-03-15 18:20:53 +0530

    a little bit styling

--- crudoperations/src/Components/InfoContainer.js
+++ crudoperations/src/Components/InfoContainer.js
@@ -22,46 +22,45 @@ const InfoContainer = ({ userData, setUserData, setEditMode }) => {
 
   console.log(userData);
   return (
-    <>
-      <button onClick={() => navigate("/form", { state: userData })}>
+    <div className="p-6">
+      <button className="p-4 mb-7 bg-orange-600 text-white rounded-lg" onClick={() => navigate("/form", { state: userData })}>
         Add User
       </button>
-      <table className="table-auto">
+      <table className="table-auto w-full">
         <thead>
           <tr>
             <th>Username</th>
             <th>Email</th>
             <th>Password</th>
             <th>Confirm Password</th>
-            <th>Opt1</th>
-            <th>Opt2</th>
+            <th>{''}</th>
+            <th>{''}</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="">
           {userData?.map((data, index) => (
             <tr key={index}>
-              <td>{data.username}</td>
-              <td>{data.email}</td>
-              <td>{data.password}</td>
-              <td>{data.confirmPassword}</td>
-              <td>
-                <button
-                  onClick={() => {
+              <td className="border-2 border-red-300 text-center">{data.username}</td>
+              <td className="border-2 border-red-300 text-center">{data.email}</td>
+              <td className="border-2 border-red-300 text-center">{data.password}</td>
+              <td className="border-2 border-red-300 text-center">{data.confirmPassword}</td>
+              <td className="text-center">
+                <button className="min-w-10 bg-blue-700 text-white p-3 rounded-lg" onClick={() => {
                     setEditMode({ id: data?.id });
                     editUserData(index);
                   }}
                 >
-                  EditIt
+                  Edit
                 </button>
               </td>
-              <td>
-                <button onClick={() => trashUserData(index)}>TrashIt</button>
+              <td className="text-center">
+                <button className="min-w-10 bg-red-700 text-white p-3 rounded-lg" onClick={() => trashUserData(index)}>Trash</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-    </>
+    </div>
   );
 };
 
